e062fa74d6be1452a604cf705ee11bff72ca4bf9
Kyle Mabry <68658192+kylemabry95@users.noreply.github.com>
2020-08-26 10:42:55 -0400

    Update Demos.py

--- Demos.py
+++ Demos.py
@@ -4,23 +4,14 @@ Goes over some of the basics of PyCharm and Python programming language.
 Last edit made: 08/25/2020
 """
 
-import mysql
+import pymysql
 
-# for loops
-x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-y = []
-for number in x:
-    z = number + 5
-    y.append(z)
+# this connects to the database that brian created.
+db = pymysql.connect('107.180.37.106', 'superRasberry', ')9Vr[dQzo8bG', 'RasberryPI')
+cursor = db.cursor()
+sql = "UPDATE `Controller` SET `response`='Hi, '\n' who is this'"
+cursor.execute(sql)
 
-# print the result of the for loop.
-print("Each element in x has 5 added to it in 'y': " + str(y))
-
-
-# mydb = mysql.connector.connect(
-#     host="107.180.37.106",
-#     user="superRasberry",
-#     password=")9Vr[dQzo8bG"
-#     )
-#
-# print(mydb)
+# We're currently doing polling, but we should do events.
+# What's the difference between polling and events? polling is a get request, like refreshing the webpage.
+# A socket
